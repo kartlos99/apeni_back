e0f855422c3c1e3c5862bfe8c65a25ad53b0d72b
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2020-06-19 17:19:14 +0400

    no orders bug fix

--- tbilisi/mobile/order/getByDate.php
+++ tbilisi/mobile/order/getByDate.php
@@ -18,50 +18,54 @@ while ($rs = mysqli_fetch_assoc($result)) {
     $orders[] = $rs;
 }
 
-$orderIDs = "";
-foreach ($orders as $order) {
-    $orderIDs .= $order['ID'] . ',';
-}
-$orderIDs = trim($orderIDs, ',');
+if (count($orders) > 0) {
 
-
-$sql = " SELECT * FROM `order_items` WHERE `orderID` IN ($orderIDs) ";
-
-$orderItems = [];
-$result = mysqli_query($con, $sql);
-while ($rs = mysqli_fetch_assoc($result)) {
-    $orderItems[] = $rs;
-}
-
-
-$sql = "
-SELECT `orderID`, `beerID`, `chek`,`canTypeID`, sum(`count`) AS `count` FROM `sales` 
-WHERE `orderID` IN ($orderIDs)
-GROUP BY `orderID`, `beerID`, `canTypeID`";
-
-$sales = [];
-$result = mysqli_query($con, $sql);
-while ($rs = mysqli_fetch_assoc($result)) {
-    $sales[] = $rs;
-}
-
-
-foreach ($orders as $index => $order) {
-    $oItems = [];
-    foreach ($orderItems as $item) {
-        if ($order['ID'] == $item['orderID']) {
-            $oItems[] = $item;
-        }
+    $orderIDs = "";
+    foreach ($orders as $order) {
+        $orderIDs .= $order['ID'] . ',';
     }
-    $oSales = [];
-    foreach ($sales as $item) {
-        if ($order['ID'] == $item['orderID']) {
-            $oSales[] = $item;
-        }
+    $orderIDs = trim($orderIDs, ',');
+
+
+    $sql = " SELECT * FROM `order_items` WHERE `orderID` IN ($orderIDs) ";
+
+    $orderItems = [];
+    $result = mysqli_query($con, $sql);
+    while ($rs = mysqli_fetch_assoc($result)) {
+        $orderItems[] = $rs;
     }
 
-    $orders[$index]['items'] = $oItems;
-    $orders[$index]['sales'] = $oSales;
+
+    $sql = "
+    SELECT `orderID`, `beerID`, `chek`,`canTypeID`, sum(`count`) AS `count` FROM `sales` 
+    WHERE `orderID` IN ($orderIDs)
+    GROUP BY `orderID`, `beerID`, `canTypeID`";
+
+    $sales = [];
+    $result = mysqli_query($con, $sql);
+    if (mysqli_num_rows($result) > 0)
+        while ($rs = mysqli_fetch_assoc($result)) {
+            $sales[] = $rs;
+        }
+
+
+    foreach ($orders as $index => $order) {
+        $oItems = [];
+        foreach ($orderItems as $item) {
+            if ($order['ID'] == $item['orderID']) {
+                $oItems[] = $item;
+            }
+        }
+        $oSales = [];
+        foreach ($sales as $item) {
+            if ($order['ID'] == $item['orderID']) {
+                $oSales[] = $item;
+            }
+        }
+
+        $orders[$index]['items'] = $oItems;
+        $orders[$index]['sales'] = $oSales;
+    }
 }
 
 $response[DATA] = $orders;
