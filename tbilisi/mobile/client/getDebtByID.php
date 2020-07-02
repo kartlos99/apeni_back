69f154f1fe513c9913c1a8e56753f9f2d162653b
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2020-07-02 10:42:46 +0400

    დავალიანება ობიექტზე

---
+++ tbilisi/mobile/client/getDebtByID.php
@@ -0,0 +1,25 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once('../connection.php');
+
+$clientID = $_GET["clientID"];
+
+$sql = "SELECT * FROM `clients_debt` WHERE `clientID` = $clientID";
+
+$result = mysqli_query($con, $sql);
+//$rs = mysqli_fetch_assoc($result);
+
+if ($result) {
+    $response[DATA] = mysqli_fetch_assoc($result);
+} else {
+    $response[SUCCESS] = false;
+    $response[ERROR_TEXT] = "can't get debt!";
+    $response[ERROR_CODE] = ER_CODE_NOT_FOUNT;
+}
+
+echo json_encode($response);
+
+mysqli_close($con);
