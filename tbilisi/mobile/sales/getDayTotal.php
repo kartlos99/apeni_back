f23e0d3fe8042e22a1763a29e388ed02f3505465
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2020-07-04 13:28:22 +0400

    დღიური რეალიზაციის ენდპოინტი

---
+++ tbilisi/mobile/sales/getDayTotal.php
@@ -0,0 +1,99 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once('../connection.php');
+
+$receivedDate = $_GET["tarigi"];
+$distrId = $_GET["distrid"];
+
+$barrelFilterByDistr = $distrId == 0 ? "" : " AND distributorID = '$distrId' ";
+
+$sqlMoney = "SELECT round(IFNULL(sum(tanxa),0),2) AS money FROM `moneyoutput` WHERE DATE(tarigi) = '$receivedDate' ";
+$sqlBarrelOutput = "
+SELECT canTypeID, SUM(backCount) AS backCount, SUM(saleCount) as saleCount from ( SELECT
+    `canTypeID`,
+    `count` AS backCount,
+    0 as saleCount
+FROM
+    `barrel_output`
+WHERE
+    DATE(`outputDate`) = '$receivedDate' $barrelFilterByDistr
+UNION
+SELECT
+    `canTypeID`,
+    0 AS backCount,
+    `count` AS saleCount
+FROM
+    `sales`
+WHERE
+    DATE(`saleDate`) = '$receivedDate' $barrelFilterByDistr
+    ) a
+GROUP by `canTypeID`
+";
+
+$sqlXarji = "SELECT * FROM `xarjebi` WHERE DATE(tarigi) = '$receivedDate'";
+
+
+$sqlSale = "
+SELECT
+    l.dasaxeleba AS beerName,
+    ROUND( SUM( s.count * s.unitPrice * k.litraji ),  2 ) AS price,
+    SUM( s.count * k.litraji) AS litraji
+FROM
+    `sales` AS s
+LEFT JOIN ludi AS l ON  s.beerID = l.id
+LEFT JOIN kasri AS k ON k.id = s.canTypeID
+WHERE
+    DATE(s.saleDate) = '$receivedDate'
+";
+
+$grouping = " GROUP BY s.beerID ";
+
+if ($distrId == 0) {
+    $sqlSale .= $grouping;
+} else {
+    // konkretuli distributori .....
+    $sqlSale .= " AND s.distributorID = '$distrId' " . $grouping;
+
+    $sqlMoney .= " AND distributor_id = '$distrId' ";
+
+    $sqlXarji .= " AND `distributor_id` = $distrId";
+}
+
+$saleArr = [];
+$saleResult = mysqli_query($con, $sqlSale);
+while ($rs = mysqli_fetch_assoc($saleResult)) {
+    $saleArr[] = $rs;
+}
+
+//$moneyArr = [];
+$moneyAmount = 0;
+$moneyResult = mysqli_query($con, $sqlMoney);
+if (mysqli_num_rows($moneyResult) > 0)
+    $moneyAmount = mysqli_fetch_assoc($moneyResult)['money'];
+
+$barrelArr = [];
+$barrelResult = mysqli_query($con, $sqlBarrelOutput);
+while ($rs = mysqli_fetch_assoc($barrelResult)) {
+    $barrelArr[] = $rs;
+}
+
+$xarjArr = [];
+$xarjResult = mysqli_query($con, $sqlXarji);
+while ($rs = mysqli_fetch_assoc($xarjResult)) {
+    $xarjArr[] = $rs;
+}
+
+$resultArr = [];
+$resultArr['sale'] = $saleArr;
+$resultArr['takenMoney'] = $moneyAmount;
+$resultArr['barrels'] = $barrelArr;
+$resultArr[XARJI] = $xarjArr;
+
+$response[DATA] = $resultArr;
+
+echo json_encode($response);
+
+mysqli_close($con);
