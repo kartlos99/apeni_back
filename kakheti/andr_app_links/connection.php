3600d825a08542253ac819f3a6b6d862155cad7d
kartlos.diakonidze <kartlos.diakonidze@wandio.com>
2021-07-30 22:47:06 +0400

    ძველი, არა საჭირო კოდის წაშლა

--- kakheti/andr_app_links/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('host','Localhost');
-define('user','apenige2_kakheti_user');
-define('pass','KlasikA01');
-define('db','apenige2_kakheti');
-//$con = mysqli_connect(host, user, pass, db) or die('db_connection_error!..');
-//mysqli_set_charset($con, "utf8");
-?>
