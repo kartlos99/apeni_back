f6a129a1bd35d2a69a83d74d8b391ac4dd8669ea
kartlos99 <kartlos99@yahoo.com>
2022-08-28 10:40:50 +0400

    receive all data about customer

---
+++ mr/mobile/client/getAllData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Apeni\JWT;
+use DataProvider;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once('../connection.php');
+checkToken();
+$clientID = $_GET["clientID"];
+$dataProvider = new DataProvider($con);
+
+$sqlGetInfo = "SELECT * FROM `customer` WHERE `id` = $clientID";
+$sqlGetPrices = "SELECT * FROM `fasebi` WHERE `obj_id` = $clientID";
+
+
+$intoResult = mysqli_query($con, $sqlGetInfo);
+$priceResult = mysqli_query($con, $sqlGetPrices);
+
+
+if ($intoResult && $priceResult) {
+
+    $prices = [];
+    while ($rs = mysqli_fetch_assoc($priceResult)) {
+        $prices[] = $rs;
+    }
+
+    $dataArr = mysqli_fetch_assoc($intoResult);
+    $dataArr['prices'] = $prices;
+    $response[DATA] = $dataArr;
+} else {
+    $response[SUCCESS] = false;
+    $response[ERROR_TEXT] = "can't get customer data!";
+    $response[ERROR_CODE] = ER_CODE_NOT_FOUNT;
+}
+
+echo json_encode($response);
+
+mysqli_close($con);
