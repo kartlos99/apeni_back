f308fd573982deed1a5d7ddb36602e4926329107
kartlos99 <kartlos99@yahoo.com>
2023-01-08 00:54:10 +0400

    fix: idle customers list flter by region

--- mr/mobile/client/getIdleInfo.php
+++ mr/mobile/client/getIdleInfo.php
@@ -7,14 +7,16 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once('../connection.php');
-checkToken();
+$sessionData = checkToken();
 
 $sqlIdleInfo = "
 SELECT `clientID`, TIMESTAMPDIFF(DAY,MAX(`saleDate`), LOCALTIMESTAMP) AS passedDays FROM `sales` s
 LEFT JOIN customer c ON c.id = s.`clientID`
-WHERE c.active = 1
+LEFT JOIN customer_to_region_map c_map ON c_map.customerID = c.id
+WHERE c.active = 1 AND c_map.regionID = {$sessionData->regionID} AND c_map.active = 1
 GROUP BY `clientID`
-HAVING passedDays > (SELECT valueInt FROM dictionary_items WHERE code = 'customer_idle_warning')";
+HAVING passedDays > (SELECT valueInt FROM dictionary_items WHERE code = 'customer_idle_warning')
+";
 
 $result = mysqli_query($con, $sqlIdleInfo);
 
