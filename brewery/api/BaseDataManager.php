a5853d0dce0206a907b7c119ff6af979cb01900f
kartlos99 <kartlos99@yahoo.com>
2023-01-22 15:16:44 +0400

    create boiling, get last boiling data

---
+++ brewery/api/BaseDataManager.php
@@ -0,0 +1,58 @@
+<?php
+
+use function Apeni\JWT\dieWithError;
+
+class BaseDataManager
+{
+
+    private $dbConn;
+
+    function __construct()
+    {
+        $this->dbConn = mysqli_connect(HOST, DB_user, DB_pass, DB_name) or die('db_connection_error!..');
+        mysqli_set_charset($this->dbConn, "utf8");
+    }
+
+    public function closeConnection()
+    {
+        mysqli_close($this->dbConn);
+    }
+
+    /**
+     * @param $query
+     * @return array
+     * base data retrieving function
+     * returns data as Array from DB according sql query
+     */
+    function getDataAsArray($query): array
+    {
+        $result = mysqli_query($this->dbConn, $query);
+        $arr = [];
+        if ($result) {
+            while ($rs = mysqli_fetch_assoc($result)) {
+                $arr[] = $rs;
+            }
+        } else {
+            dieWithError(CUSTOM_HTTP_ERROR_CODE, mysqli_error($this->dbConn));
+        }
+        return $arr;
+    }
+
+    /**
+     * @param $insertSql
+     * @return array
+     * base function for data inserting
+     */
+    protected function baseInsert($insertSql): array
+    {
+        $result = mysqli_query($this->dbConn, $insertSql);
+        if (!$result)
+            dieWithError(CUSTOM_HTTP_ERROR_CODE, mysqli_error($this->dbConn));
+        return [RECORD_ID_KEY => mysqli_insert_id($this->dbConn)];
+    }
+
+    protected function baseDelete($deleteSql)
+    {
+        return mysqli_query($this->dbConn, $deleteSql);
+    }
+}
---
+++ brewery/api/boiler/getPreviousParams.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Apeni\JWT;
+
+use BoilerDataManager;
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once('../load.php');
+$sessionData = checkToken();
+$userID = $sessionData->userID;
+
+$json = file_get_contents('php://input');
+$postData = json_decode($json);
+
+$dbManager = new BoilerDataManager();
+
+$beerID = $postData->beerID;
+
+$defaultNextID = $dbManager->nextBoilingID();
+
+$fullData = $dbManager->getLastBoilingData($beerID);
+//echo json_encode($fullData);
+$boilingID = $fullData["ID"];
+$fullData["waterList"] = $dbManager->getWaterData($boilingID);
+$fullData["saltList"] = $dbManager->getSaltData($boilingID);
+$fullData["maltList"] = $dbManager->getMaltData($boilingID);
+$fullData["hopsList"] = $dbManager->getHopsData($boilingID);
+$fullData["delayList"] = $dbManager->getDelayData($boilingID);
+$fullData["filteringList"] = $dbManager->getFilteringData($boilingID);
+$fullData["defaultNumber"] = $defaultNextID;
+echo json_encode($fullData);
+
+$dbManager->closeConnection();
