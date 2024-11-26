d522ae9be7249ec4e6716ddcb17702a6ad6eab68
Kartlos <kartlos99@yahoo.com>
2024-11-26 22:16:49 +0400

    distribution: new api   - get customers edle info endpoint

---
+++ mr/mobileApi/customer/idleInfo.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Apeni\JWT;
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once('../connection.php');
+$sessionData = checkToken();
+require_once('../../BaseDbManagerV2.php');
+
+$dbManager = new \BaseDbManagerV2();
+
+$sqlIdleInfo = "
+SELECT `clientID`, TIMESTAMPDIFF(DAY,MAX(`saleDate`), LOCALTIMESTAMP) AS passedDays FROM `sales` s
+LEFT JOIN customer c ON c.id = s.`clientID`
+LEFT JOIN customer_to_region_map c_map ON c_map.customerID = c.id
+WHERE c.active = 1 AND c_map.regionID = {$sessionData->regionID} AND c_map.active = 1
+GROUP BY `clientID`
+HAVING passedDays > (SELECT valueInt FROM dictionary_items WHERE code = 'customer_idle_warning')
+";
+
+echo json_encode(
+    $dbManager->getDataAsArray($sqlIdleInfo)
+);
+
+$dbManager->closeConnection();
